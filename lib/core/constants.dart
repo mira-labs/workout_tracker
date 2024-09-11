6df4e2258e25b0d5210bd49d8d7362f126d6ca7a
Radical Rad <rad@mira-labs.com>
2024-09-11 01:50:49 +0100

    Values for select boxes

---
+++ lib/core/constants.dart
@@ -0,0 +1,23 @@
+// lib/core/constants.dart
+
+const List<String> exercises = [
+  'Squat',
+  'Bench Press',
+  'Deadlift',
+  'Overhead Press',
+];
+
+const List<int> repsList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const List<double> weights = [
+  5.0,
+  10.0,
+  15.0,
+  20.0,
+  25.0,
+  30.0,
+  35.0,
+  40.0,
+  45.0,
+  50.0,
+];
